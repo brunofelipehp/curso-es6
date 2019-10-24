106cd3ef895ec4cdd482aa259513789fa7b54d86
Bruno Felipe <brunofelipehp8@gmail.com>
2019-10-24 13:10:28 -0300

    Operações em Array

--- bundle.js
+++ bundle.js
@@ -1,26 +1,23 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var arr = [1, 3, 4, 5, 8, 9]; //percorre todos os items do vetor
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr); // retorna o valor total da soma de todos os items do vetor 
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum); //retorna true ou false da condição passada
 
-var Matematica =
-/*#__PURE__*/
-function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
-  }
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter); // retorna o valor encotrando 
 
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(a, b) {
-      return a + b;
-    }
-  }]);
-
-  return Matematica;
-}();
-
-console.log(Matematica.soma(2, 2));
+var find = arr.find(function (item) {
+  return item === 4;
+});
+console.log(find);
--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-class Matematica{
+const  arr = [1, 3, 4, 5, 8, 9];
 
-    static soma(a, b){
-        return a + b;
-    }
+//percorre todos os items do vetor
+const newArr =  arr.map(function(item, index){
+    return item + index;
+});
 
-    
-}
+console.log(newArr);
+// retorna o valor total da soma de todos os items do vetor 
+const sum = arr.reduce(function(total, next){
+    return total + next;
+});
 
-console.log(Matematica.soma(2, 2));
+console.log(sum);
+//retorna true ou false da condição passada
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+
+// retorna o valor encotrando 
+const find = arr.find(function(item){
+    return item === 4;
+});
+
+console.log(find);
