57cc0298d8a9a533330dbaf04fb59ec64b3359f4
Bruno Felipe <brunofelipehp8@gmail.com>
2019-11-08 18:00:18 -0300

    Loading

--- src/main.js
+++ src/main.js
@@ -12,37 +12,65 @@ class App {
         this.registerHandlers();
     }
 
-    registerHandlers(){
+    registerHandlers() {
 
         this.formEl.onsubmit = event => this.addRepository(event);
-    
+
     }
 
-  async addRepository(event) {
+    //Carregamento 
+    setLoading(loading = true){
+
+        if (loading === true) {
+            let loadingEl =  document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando....'));
+            loadingEl.setAttribute('id', 'loading');
+            
+            this.formEl.appendChild(loadingEl);
+        }else{
+            document.getElementById('loading').remove();
+        }
+
+    }
+// Busca os repositorios da api do github
+    async addRepository(event) {
         event.preventDefault();
 
         const repoInput = this.inputEl.value;
 
-        if(repoInput === 0)
-        return;
+        if (repoInput === 0)
+            return;
 
-        const response = await api.get(`/repos/${repoInput}`);
+            this.setLoading();
 
-       const {name, description, html_url, owner: { avatar_url}} = response.data;
+        try {
 
-        this.repositories.push({
-            name, 
-            description,
-            avatar_url,
-            html_url,
-        });
+            const response = await api.get(`/repos/${repoInput}`);
 
-        this.inputEl.value = '';
+            const { name, description, html_url, owner: { avatar_url } } = response.data;
 
-        this.render();
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+
+            this.inputEl.value = '';
+
+            this.render();
+
+        } catch (err) {
+
+            alert('O repositório não existe');
+
+        }
+
+        this.setLoading(false);
     }
 
-    render(){
+    // renderiza os repositorio buscado em tela
+    render() {
         this.listEl.innerHTML = '';
 
         this.repositories.forEach(repo => {
@@ -52,7 +80,7 @@ class App {
             let titleEl = document.createElement('strong');
             titleEl.appendChild(document.createTextNode(repo.name));
 
-            let descriptionEl  = document.createElement('p');
+            let descriptionEl = document.createElement('p');
             descriptionEl.appendChild(document.createTextNode(repo.description));
 
             let linkEl = document.createElement('a');
@@ -60,12 +88,12 @@ class App {
             linkEl.setAttribute('href', repo.html_url);
             linkEl.appendChild(document.createTextNode('Acessar'));
 
-            let listItemEl  = document.createElement('li');
+            let listItemEl = document.createElement('li');
             listItemEl.appendChild(imgEl);
             listItemEl.appendChild(titleEl);
             listItemEl.appendChild(descriptionEl);
             listItemEl.appendChild(linkEl);
-            
+
             this.listEl.appendChild(listItemEl);
         });
     }
